212fe98442feb5d62b838b9132caefbfb2ce8487
Khaled Badran <badrankhaled97@gmail.com>
2021-11-08 23:48:36 -0500

    update merge_csvs script to read from the correct directory

--- scripts/merge_csvs.py
+++ scripts/merge_csvs.py
@@ -1,14 +1,18 @@
+"""
+This script merges all the test smell csvs in the repos_csvs directory and output a single csv file
+"""
+
 import glob
 from pathlib import Path
 import os
 import pandas as pd
 
 
-csvs_dir = Path('..', 'data', 'sample_csvs')
+csvs_dir = Path('..', 'data', 'repos_csvs')
+
 all_files = glob.glob(os.path.join(csvs_dir, "*.csv"))
 
 all_dfs = (pd.read_csv(f, index_col=None, header=0) for f in all_files)
 merged_df = pd.concat(all_dfs, ignore_index=True)
 
-merged_df.to_csv('delete_me.csv')
-print(merged_df)
+merged_df.to_csv(Path(csvs_dir, 'merged_csvs.csv'))
